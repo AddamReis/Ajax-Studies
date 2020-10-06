1cb44023dfbf911769765c5a358e2da4709ab25b
Addam Reis <addamreis@gmail.com>
2020-10-05 23:11:24 -0300

    Config Server

---
+++ ajax/ajax/server.js
@@ -0,0 +1,12 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+const app = express()
+
+/*aplicado para qualquer requisição do servidor*/
+app.use(express.static('.')) //provém os arquivos estáticos da aplicação a partir da raiz atual na pasta
+app.use(bodyParser.urlencoded({ extended: true })) //se receber um submit de fomulário transforma em objeto
+app.use(bodyParser.json()) //se receber json transforma em objeto
+
+app.get('/teste', (req, res) => res.send('Ok'))
+
+app.listen(8081, () => console.log('Executando...'))
