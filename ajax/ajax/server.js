e31c6f07e70df4c6fbf892cddd294d0a4a89033f
Addam Reis <addamreis@gmail.com>
2020-10-06 18:52:08 -0300

    Xml Http Request Part 2 Upload

--- ajax/ajax/server.js
+++ ajax/ajax/server.js
@@ -7,4 +7,27 @@ app.use(express.static('.')) //provém os arquivos estáticos da aplicação a p
 app.use(bodyParser.urlencoded({ extended: true })) //se receber um submit de fomulário transforma em objeto
 app.use(bodyParser.json()) //se receber json transforma em objeto
 
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) {
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) {
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
 app.listen(8081, () => console.log('Executando...'))
